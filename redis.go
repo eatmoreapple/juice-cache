9a93b583d5f0626da56d8b46efd50942e8d84177
eatmoreapple <eatmoreorange@gmail.com>
2023-05-24 10:59:15 +0800

    warp marshal store value

--- redis.go
+++ redis.go
@@ -2,6 +2,7 @@ package juice_cache
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"github.com/eatmoreapple/juice/cache"
 	"github.com/google/uuid"
@@ -22,7 +23,11 @@ func (r *RedisCache) Set(ctx context.Context, key string, value any) error {
 	case <-ctx.Done():
 		return ctx.Err()
 	default:
-		return r.client.HSet(ctx, r.uuid, key, value).Err()
+		data, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		return r.client.HSet(ctx, r.uuid, key, data).Err()
 	}
 }
 
@@ -32,14 +37,14 @@ func (r *RedisCache) Get(ctx context.Context, key string, dst any) error {
 	case <-ctx.Done():
 		return ctx.Err()
 	default:
-		// dst must implement encoding.BinaryUnmarshaler if it is not basic type.
-		err := r.client.HGet(ctx, r.uuid, key).Scan(dst)
+		var data []byte
+		err := r.client.HGet(ctx, r.uuid, key).Scan(&data)
 		if err != nil {
 			if errors.Is(err, redis.Nil) {
 				return cache.ErrCacheNotFound
 			}
 		}
-		return err
+		return json.Unmarshal(data, dst)
 	}
 }
 
